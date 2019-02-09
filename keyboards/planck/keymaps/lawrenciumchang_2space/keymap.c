b7e415ad1cc401d08c864d87f2e8ba2db48494d9
Lawrence Chang <lawrencium.chang@gmail.com>
2019-02-08 23:31:40 -0600

    updated 2space keymapping to use LAYOUT_planck_2x2u definition

--- keyboards/planck/keymaps/lawrenciumchang_2space/keymap.c
+++ keyboards/planck/keymaps/lawrenciumchang_2space/keymap.c
@@ -30,11 +30,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |   L2  | Ctrl | Alt  | GUI  |    Enter    |    Space    | GUI  | Alt  | Ctrl |  L2   |
  * `-------------------------------------------------------------------------------------'
  */
-[_BASE] = LAYOUT_planck_grid(
+[_BASE] = LAYOUT_planck_2x2u(
     KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     L1,            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT(L1, KC_QUOT),
     SFT_T(KC_ESC), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT ,
-    L2,            KC_LCTL, KC_LALT, KC_LGUI, KC_ENT,  KC_ENT,  KC_SPC,  KC_SPC,  KC_RGUI, KC_RALT, KC_CAPS, L2
+    L2,            KC_LCTL, KC_LALT, KC_LGUI, KC_ENT,           KC_SPC,           KC_RGUI, KC_RALT, KC_CAPS, L2
 ),
 
 /* Layer 1
@@ -48,11 +48,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |  __   |  __  |  __  |  __  |      __     |      __     |  __  |  __  |  __  |  __   |
  * `-------------------------------------------------------------------------------------'
  */
-[_L1] = LAYOUT_planck_grid(
+[_L1] = LAYOUT_planck_2x2u(
     __, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_UP,   KC_LPRN, KC_RPRN, KC_DEL,
 		__, KC_BSLS, KC_GRV,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_LEFT, KC_DOWN, KC_RGHT, KC_ASTR, __,
 		__, KC_PIPE, KC_TILD, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, __,      __,      __,      __,      __,
-		__, __,      __,      __,      __,      __,      __,      __,      __,      __,      __,      __
+		__, __,      __,      __,      __,               __,               __,      __,      __,      __
 ),
 
 /* Layer 2
@@ -66,11 +66,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |  __   |  __  |  __  |  __  |      __     |      __     |  0   |  0   |  .   |  __   |
  * `-------------------------------------------------------------------------------------'
  */
-[_L2] = LAYOUT_planck_grid(
+[_L2] = LAYOUT_planck_2x2u(
     __, __,      __,      __,      __,      __, __,   __, KC_7, KC_8, KC_9,   KC_SLEP,
 		__, __,      __,      __,      __,      __, __,   __, KC_4, KC_5, KC_6,   KC_VOLU,
 		__, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, __, __,   __, KC_1, KC_2, KC_3,   KC_VOLD,
-		__, __,      __,      __,      __,      __, __,   __, KC_0, KC_0, KC_DOT, __
+		__, __,      __,      __,      __,          __,       KC_0, KC_0, KC_DOT, __
 )
 
 };
