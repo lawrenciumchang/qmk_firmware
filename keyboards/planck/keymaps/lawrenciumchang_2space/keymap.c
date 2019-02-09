db83bbd30b7678cf413341234f30a14e1738bbec
Lawrence Chang <lawrencium.chang@gmail.com>
2019-02-09 16:43:15 -0600

    updates to planck 2space

--- keyboards/planck/keymaps/lawrenciumchang_2space/keymap.c
+++ keyboards/planck/keymaps/lawrenciumchang_2space/keymap.c
@@ -27,14 +27,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-------+------+------+------+------+------|------+------+------+------+------+-------|
  * |Sft/Esc|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift |
  * |-------+------+------+------+------+------+------+------+------+------+------+-------|
- * |   L2  | Ctrl | Alt  | GUI  |    Enter    |    Space    | GUI  | Alt  | Caps |  L2   |
+ * |   L2  | Ctrl | Alt  | GUI  |  Enter/L2   |  Space/L2   | GUI  | Alt  | Ctrl | Caps  |
  * `-------------------------------------------------------------------------------------'
  */
 [_BASE] = LAYOUT_planck_grid(
-    KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    L1,            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT(L1, KC_QUOT),
-    SFT_T(KC_ESC), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT ,
-    L2,            KC_LCTL, KC_LALT, KC_LGUI, KC_ENT,  KC_ENT,  KC_SPC,  KC_SPC,  KC_RGUI, KC_RALT, KC_CAPS, L2
+    KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,    KC_O,    KC_P,    KC_BSPC,
+    L1,            KC_A,    KC_S,    KC_D,    KC_F,           KC_G,           KC_H,           KC_J,           KC_K,    KC_L,    KC_SCLN, LT(L1, KC_QUOT),
+    SFT_T(KC_ESC), KC_Z,    KC_X,    KC_C,    KC_V,           KC_B,           KC_N,           KC_M,           KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    L2,            KC_LCTL, KC_LALT, KC_LGUI, LT(L2, KC_ENT), LT(L2, KC_ENT), LT(L2, KC_SPC), LT(L2, KC_SPC), KC_RGUI, KC_RALT, KC_RCTL, KC_CAPS
 ),
 
 /* Layer 1
@@ -57,20 +57,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Layer 2
  * ,-------------------------------------------------------------------------------------.
- * |  __   |  __  |  __  |  __  |  __  |  __  |  __  |  __  |  7   |  8   |  9   | Sleep |
+ * |  __   |  1   |  2   |  3   |  4   |  5   |  6   |  7   |  8   |  9   |  __  | Sleep |
  * |-------+------+------+------+------+-------------+------+------+------+------+-------|
- * |  __   |  __  |  __  |  __  |  __  |  __  |  __  |  __  |  4   |  5   |  6   | VolUp |
+ * |  __   |  __  |  __  |  __  |  __  |  __  |  .   |  4   |  5   |  6   |BrtUp | VolUp |
  * |-------+------+------+------+------+------|------+------+------+------+------+-------|
- * |  __   | Prev | Play | Next | Mute |  __  |  __  |  __  |  1   |  2   |  3   |VolDown|
+ * |  __   | Prev | Play | Next | Mute |  __  |  0   |  1   |  2   |  3   |BrtDwn|VolDown|
  * |-------+------+------+------+------+------+------+------+------+------+------+-------|
- * |  __   |  __  |  __  |  __  |      __     |      __     |  0   |  0   |  .   |  __   |
+ * |  __   |  __  |  __  |  __  |      __     |      __     |  __  |  __  |  __  |  __   |
  * `-------------------------------------------------------------------------------------'
  */
 [_L2] = LAYOUT_planck_grid(
-    __, __,      __,      __,      __,      __, __,   __, KC_7, KC_8, KC_9,   KC_SLEP,
-		__, __,      __,      __,      __,      __, __,   __, KC_4, KC_5, KC_6,   KC_VOLU,
-		__, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, __, __,   __, KC_1, KC_2, KC_3,   KC_VOLD,
-		__, __,      __,      __,      __,      __, __,   __, KC_0, KC_0, KC_DOT, __
+    __, KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_6,   KC_7, KC_8, KC_9, __,      KC_SLEP,
+		__, __,      __,      __,      __,      __,   KC_DOT, KC_4, KC_5, KC_6, KC_PAUS, KC_VOLU,
+		__, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, __,   KC_0,   KC_1, KC_2, KC_3, KC_SLCK, KC_VOLD,
+		__, __,      __,      __,      __,      __,   __,     __,   __,   __,   __,      __
 )
 
 };
