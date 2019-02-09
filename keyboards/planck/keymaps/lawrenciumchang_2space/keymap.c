ab44c42cc00dc875987e4bbd21eb5c8a38394d3e
Lawrence Chang <lawrencium.chang@gmail.com>
2019-02-09 17:08:48 -0600

    updates to both planck keymappings

--- keyboards/planck/keymaps/lawrenciumchang_2space/keymap.c
+++ keyboards/planck/keymaps/lawrenciumchang_2space/keymap.c
@@ -27,14 +27,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-------+------+------+------+------+------|------+------+------+------+------+-------|
  * |Sft/Esc|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift |
  * |-------+------+------+------+------+------+------+------+------+------+------+-------|
- * |   L2  | Ctrl | Alt  | GUI  |  Enter/L2   |  Space/L2   | GUI  | Alt  | Ctrl | Caps  |
+ * |   L2  | Ctrl | Alt  | GUI  |    Enter    |    Space    | GUI  | Alt  | Caps |  L2   |
  * `-------------------------------------------------------------------------------------'
  */
 [_BASE] = LAYOUT_planck_grid(
-    KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,    KC_O,    KC_P,    KC_BSPC,
-    L1,            KC_A,    KC_S,    KC_D,    KC_F,           KC_G,           KC_H,           KC_J,           KC_K,    KC_L,    KC_SCLN, LT(L1, KC_QUOT),
-    SFT_T(KC_ESC), KC_Z,    KC_X,    KC_C,    KC_V,           KC_B,           KC_N,           KC_M,           KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    L2,            KC_LCTL, KC_LALT, KC_LGUI, LT(L2, KC_ENT), LT(L2, KC_ENT), LT(L2, KC_SPC), LT(L2, KC_SPC), KC_RGUI, KC_RALT, KC_RCTL, KC_CAPS
+    KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC,
+    L1,            KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, LT(L1, KC_QUOT),
+    SFT_T(KC_ESC), KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    L2,            KC_LCTL, KC_LALT, KC_LGUI, KC_ENT, KC_ENT, KC_SPC, KC_SPC, KC_RGUI, KC_RALT, KC_CAPS, L2
 ),
 
 /* Layer 1
