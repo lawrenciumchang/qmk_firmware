398350e28c3c82bead2a85f630196655aa46beb6
Lawrence Chang <lawrencium.chang@gmail.com>
2019-09-02 10:48:55 -0500

    [crkbd] updated placement of control and caps

--- keyboards/crkbd/keymaps/lawrenciumchang/config.h
+++ keyboards/crkbd/keymaps/lawrenciumchang/config.h
@@ -34,7 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_FORCE_HOLD
 #define PERMISSIVE_HOLD
-#define IGNORE_MOD_TAP_INTERRUPT
+// #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_TERM 120
 
 #undef RGBLED_NUM
