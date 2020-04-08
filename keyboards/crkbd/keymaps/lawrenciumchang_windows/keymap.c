edf7056f9c228a0d19f1aafcb7eb07366ccbe2fc
Lawrence Chang <lawrencium.chang@gmail.com>
2020-04-08 10:45:38 -0500

    [crkbd] added windows layout and gaming layer

---
+++ keyboards/crkbd/keymaps/lawrenciumchang_windows/keymap.c
@@ -0,0 +1,195 @@
+#include QMK_KEYBOARD_H
+#include "bootloader.h"
+#ifdef PROTOCOL_LUFA
+  #include "lufa.h"
+  #include "split_util.h"
+#endif
+#ifdef SSD1306OLED
+  #include "ssd1306.h"
+#endif
+
+extern keymap_config_t keymap_config;
+
+extern uint8_t is_master;
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _BASE 0
+#define _LOWER 3
+#define _RAISE 4
+
+enum custom_keycodes {
+  BASE,
+  LOWER,
+  RAISE
+};
+
+#define KC___     KC_TRNS
+#define KC_LOWER  LOWER
+#define KC_RAISE  RAISE
+
+#define KC_SFTT   SFT_T(KC_ESC)
+
+#define KC_GAME   TG(_GAME)
+#define KC_BASE   TG(_BASE)
+#define KC_NAV    MO(_NAV)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT_kc( \
+  //,-----------------------------------------.                ,-----------------------------------------.
+        TAB,     Q,     W,     E,     R,     T,                      Y,     U,     I,     O,     P,  BSPC,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+      LOWER,     A,     S,     D,     F,     G,                      H,     J,     K,     L,  SCLN, QUOTE,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+       SFTT,     Z,     X,     C,     V,     B,                      N,     M,  COMM,   DOT,  SLSH,  RSFT,\
+  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
+                                   LCTL, RAISE,   ENT,      SPC, LOWER, RGUI \
+                              //`--------------------'  `--------------------'
+  ),
+
+  [_LOWER] = LAYOUT_kc( \
+  //,-----------------------------------------.                ,-----------------------------------------.
+         __,  EXLM,    AT,  HASH,   DLR,  PERC,                   CIRC,  AMPR,    UP,  LPRN,  RPRN,   DEL,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+         __,  BSLS,   GRV,  MINS,   EQL,  LBRC,                   RBRC,  LEFT,  DOWN, RIGHT,  ASTR,  RALT,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+         __,  PIPE,  TILD,  UNDS,  PLUS,  LCBR,                   RCBR,  SLCK,  PAUS,  VOLD,  VOLU,    __,\
+  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
+                                   LALT,   SPC,   SPC,       __,    __, GAME \
+                              //`--------------------'  `--------------------'
+  ),
+
+  [_RAISE] = LAYOUT_kc( \
+  //,-----------------------------------------.                ,-----------------------------------------.
+         __,     1,    2 ,     3,     4,     5,                      6,     7,     8,     9,     0,  BSPC,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+         __,  SLCK,  PAUS,  VOLD,  VOLU,    __,                    DOT,     4,     5,     6,    __,  CAPS,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+         __,  MPRV,  MPLY,  MNXT,  MUTE,    __,                      0,     1,     2,     3,    __,    __,\
+  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
+                                     __,    __,    __,       __,    __,   __ \
+                              //`--------------------'  `--------------------'
+  ),
+
+  [_GAME] = LAYOUT_kc( \
+  //,-----------------------------------------.                ,-----------------------------------------.
+        __,    __,     W,     E,    __,    __,                     __,    __,     I,     O,    __,    __,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+       NAV,    __,    __,     S,     D,    __,                     __,     K,     L,    __,    __,   SPC,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+        __,    __,    __,    __,    __,    __,                     __,    __,    __,    __,    __,    __,\
+  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
+                                    __,   ENT,     F,        J,   NAV,  BASE \
+                              //`--------------------'  `--------------------'
+  ),
+
+  [_NAV] = LAYOUT_kc( \
+  //,-----------------------------------------.                ,-----------------------------------------.
+        __,    __,    __,    __,    __,    __,                     __,    __,    UP,    __,    __,    __,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+        __,    __,    __,    __,    __,    __,                     __,  LEFT,  DOWN, RIGHT,    __,    __,\
+  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
+        __,    __,    __,    __,    __,    __,                     __,    __,    __,    __,    __,    __,\
+  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
+                                    __,    __,    __,       __,    __,    __ \
+                              //`--------------------'  `--------------------'
+  )
+
+};
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+void matrix_init_user(void) {
+    //SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
+    #ifdef SSD1306OLED
+        iota_gfx_init(!has_usb());   // turns on the display
+    #endif
+}
+
+//SSD1306 OLED update loop, make sure to add #define SSD1306OLED in config.h
+#ifdef SSD1306OLED
+
+// When add source files to SRC in rules.mk, you can use functions.
+const char *read_layer_state(void);
+const char *read_logo(void);
+void set_keylog(uint16_t keycode, keyrecord_t *record);
+const char *read_keylog(void);
+const char *read_keylogs(void);
+
+// const char *read_mode_icon(bool swap);
+// const char *read_host_led_state(void);
+// void set_timelog(void);
+// const char *read_timelog(void);
+
+void matrix_scan_user(void) {
+   iota_gfx_task();
+}
+
+void matrix_render_user(struct CharacterMatrix *matrix) {
+  if (is_master) {
+    // If you want to change the display of OLED, you need to change here
+    matrix_write_ln(matrix, read_layer_state());
+    matrix_write_ln(matrix, read_keylog());
+    matrix_write_ln(matrix, read_keylogs());
+    //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
+    //matrix_write_ln(matrix, read_host_led_state());
+    //matrix_write_ln(matrix, read_timelog());
+  } else {
+    matrix_write(matrix, read_logo());
+  }
+}
+
+void matrix_update(struct CharacterMatrix *dest, const struct CharacterMatrix *source) {
+  if (memcmp(dest->display, source->display, sizeof(dest->display))) {
+    memcpy(dest->display, source->display, sizeof(dest->display));
+    dest->dirty = true;
+  }
+}
+
+void iota_gfx_task_user(void) {
+  struct CharacterMatrix matrix;
+  matrix_clear(&matrix);
+  matrix_render_user(&matrix);
+  matrix_update(&display, &matrix);
+}
+#endif//SSD1306OLED
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    #ifdef SSD1306OLED
+        set_keylog(keycode, record);
+    #endif
+        // set_timelog();
+  }
+
+  switch (keycode) {
+    case BASE:
+      if (record->event.pressed) {
+        persistent_default_layer_set(1UL<<_BASE);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+      } else {
+        layer_off(_LOWER);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+      } else {
+        layer_off(_RAISE);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
