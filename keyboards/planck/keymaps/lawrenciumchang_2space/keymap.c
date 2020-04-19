8f73fb02f243e2f5999f710803fc03c5a3873e5d
Lawrence Chang <lawrencium.chang@gmail.com>
2020-04-19 13:11:03 -0500

    [planck] changed gui keycode to cmd to fix command key on mac

--- keyboards/planck/keymaps/lawrenciumchang_2space/keymap.c
+++ keyboards/planck/keymaps/lawrenciumchang_2space/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC,
     L1,            KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, LT(L1, KC_QUOT),
     SFT_T(KC_ESC), KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    L2,            KC_LCTL, KC_LALT, KC_LGUI, KC_ENT, KC_ENT, KC_SPC, KC_SPC, KC_RGUI, KC_RALT, KC_CAPS, L2
+    L2,            KC_LCTL, KC_LALT, KC_LCMD, KC_ENT, KC_ENT, KC_SPC, KC_SPC, KC_RCMD, KC_RALT, KC_CAPS, L2
 ),
 
 /* Layer 1
