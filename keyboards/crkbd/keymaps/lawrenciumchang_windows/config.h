7264f1d6016da2ea3f61b6bfbe37c5cff5cb2039
Lawrence Chang <lchang@credera.com>
2020-06-23 13:15:24 -0500

    Revert "[crkbd] lowered tapping term for windows config"
    This reverts commit 66573b0e8813cfbbc9362ff84d8d801c9d2bfc43.

--- keyboards/crkbd/keymaps/lawrenciumchang_windows/config.h
+++ keyboards/crkbd/keymaps/lawrenciumchang_windows/config.h
@@ -35,7 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_FORCE_HOLD
 #define PERMISSIVE_HOLD
 // #define IGNORE_MOD_TAP_INTERRUPT
-#define TAPPING_TERM 80
+#define TAPPING_TERM 120
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
